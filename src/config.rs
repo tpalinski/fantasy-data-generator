188572feff6f4f12690ec8402fd04917e3cd03f2
mrochny <tpalinski@icloud.com>
2023-11-04 12:40:41 +0100

    Add config file handling

---
+++ src/config.rs
@@ -0,0 +1,55 @@
+use std::fs;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+struct ConfigData {
+    pub first_period: FirstPeriod,
+}
+
+#[derive(Serialize, Deserialize)]
+struct FirstPeriod {
+    pub adventurers: u64,
+    pub destinations: u64,
+}
+
+impl ConfigData {
+    pub fn new() -> Self {
+        ConfigData { first_period: FirstPeriod { adventurers: 0, destinations: 0 } }
+    }
+}
+
+
+#[derive(Serialize, Deserialize)]
+pub struct ConfigReader {
+    config_path: String,
+    config_data: ConfigData,
+}
+
+impl ConfigReader {
+    pub fn new(path: &str) -> Self {
+        ConfigReader { config_path: String::from(path), config_data: ConfigData::new() } }
+
+    pub fn get_config(&mut self) {
+       let contents = match fs::read_to_string(&self.config_path) {
+            Ok(c) => c,
+            Err(_) => {
+                panic!("Error reading config file")
+            }
+        };
+        let config_data: ConfigData = match toml::from_str(&contents) {
+            Ok(d) => d,
+            Err(_) => {
+                panic!("Error deserializing config file")
+            }
+        };
+        self.config_data = config_data;
+    }
+
+    pub fn get_first_adventurers(self) -> u64{
+        self.config_data.first_period.adventurers
+    }
+
+    pub fn get_first_destinations(self) -> u64 {
+        self.config_data.first_period.destinations
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-mod adventurers;
-pub mod letter_data;
-pub mod model;
+mod adventurers; pub mod letter_data; pub mod model;
+mod config;
 use std::time::{Duration, Instant};
 use adventurers::AdventurerGenerator;
 
+use crate::config::ConfigReader;
+
 fn main() {
+    let mut config_data = ConfigReader::new("config.toml");
+    config_data.get_config();
     let start = Instant::now();
-    let adv = AdventurerGenerator::new(200000);
+    let adv = AdventurerGenerator::new(config_data.get_first_adventurers());
     adv.generate();
     let duration = start.elapsed();
     println!("Generated users in {:?}", duration)
