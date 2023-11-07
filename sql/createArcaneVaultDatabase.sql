116ee2a4a1ee1d9b065944d88bc70ae840485e8e
AdamNiesiobedzki <adam.niesiobedzki@o2.pl>
2023-11-07 23:48:21 +0100

    bulk v1

--- sql/createArcaneVaultDatabase.sql
+++ sql/createArcaneVaultDatabase.sql
@@ -4,15 +4,15 @@ go
 
 use arcane_vault
 
-CREATE TABLE travelers (
-    traveler_id  INT IDENTITY(1,1) PRIMARY KEY,
-    first_name NVARCHAR(20),
-    last_name NVARCHAR(30),
+CREATE TABLE adventurers (
+    adventurer_id  INT PRIMARY KEY,
+    name NVARCHAR(20),
+    surname NVARCHAR(30),
     level TINYINT CHECK (level >= 1 AND level <=20),
 );
 
 CREATE TABLE facilities (
-    facility_id INT IDENTITY(1,1) PRIMARY KEY,
+    facility_id INT PRIMARY KEY,
     facility_address NVARCHAR(75),
 );
 
@@ -23,40 +23,41 @@ CREATE TABLE civilizations(
     religion NVARCHAR(30),
 );
 
-CREATE TABLE expedition_destinations(
-    place_id INT IDENTITY(1,1) PRIMARY KEY,
-    place_name NVARCHAR(50),
-    coordinates GEOGRAPHY,
-    civilization_name NVARCHAR(50),
-    FOREIGN KEY (civilization_name) REFERENCES civilizations(civilization_name) ON DELETE CASCADE,
+CREATE TABLE destinations(
+    destination_id INT PRIMARY KEY,
+    name NVARCHAR(75),
+    coordinates NVARCHAR(20),
+    difficulty TINYINT CHECK (difficulty >= 1 AND difficulty <= 20),
+    civilization NVARCHAR(50),
+    FOREIGN KEY (civilization) REFERENCES civilizations(civilization_name) ON DELETE CASCADE,
 );
 
-CREATE TABLE expeditions(
-    expedition_id INT IDENTITY(1,1) PRIMARY KEY,
-    start_date DATE,
+CREATE TABLE adventures(
+    adventure_id INT PRIMARY KEY,
+    facility_id INT,
+    destination_id INT,
+    date DATE,
     deadline DATE,
     status NVARCHAR(30) CHECK (status IN ('planned', 'in progress', 'finished', 'failed')),
-    expense INT,
-    facility_id INT,
+    cost INT,
     FOREIGN KEY (facility_id) REFERENCES facilities(facility_id) ON DELETE CASCADE,
-    place_id INT,
-    FOREIGN KEY (place_id) REFERENCES expedition_destinations(place_id) ON DELETE CASCADE,
+    FOREIGN KEY (destination_id) REFERENCES destinations(destination_id) ON DELETE CASCADE,
 );
 
 CREATE TABLE artifacts(
-    artifact_id INT IDENTITY(1,1) PRIMARY KEY,
+    artifact_id INT PRIMARY KEY,
+    adventure_id INT,
     status NVARCHAR(30) CHECK (status IN ('Unidentfied', 'Identified', 'Sold', 'Stolen', 'Broken')),
-    type NVARCHAR(50),
-    expedition_id INT,
-    FOREIGN KEY (expedition_id) REFERENCES expeditions(expedition_id) ON DELETE CASCADE,
+    kind NVARCHAR(50),
+    FOREIGN KEY (adventure_id) REFERENCES adventures(adventure_id) ON DELETE CASCADE,
 );
 
-CREATE TABLE expedition_participations(
-    traveler_id INT,
-    expedition_id INT,
-    FOREIGN KEY (traveler_id) REFERENCES travelers(traveler_id) ON DELETE CASCADE,
-    FOREIGN KEY (expedition_id) REFERENCES expeditions(expedition_id) ON DELETE CASCADE,
-    PRIMARY KEY (traveler_id, expedition_id),
+CREATE TABLE participations(
+    adventurer_id INT,
+    adventure_id INT,
+    FOREIGN KEY (adventurer_id) REFERENCES adventurers(adventurer_id) ON DELETE CASCADE,
+    FOREIGN KEY (adventure_id) REFERENCES adventures(adventure_id) ON DELETE CASCADE,
+    PRIMARY KEY (adventurer_id, adventure_id),
 );
 
 
--- src/model/civilization.rs
+++ src/model/civilization.rs
@@ -2,18 +2,18 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct Civilization {
-    name: String,
+    civilization_name: String,
     race: String,
     period: i32,
     religion: String
 } 
 
 impl Civilization {
-    pub fn new(name: String, race: String, period: i32, religion: String) -> Self {
-        Civilization { name, race, period, religion}
+    pub fn new(civilization_name: String, race: String, period: i32, religion: String) -> Self {
+        Civilization { civilization_name, race, period, religion}
     }
 
     pub fn get_name(&self) -> String {
-        self.name.to_string()
+        self.civilization_name.to_string()
     }
 }
